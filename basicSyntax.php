0254013779cacc3b5d6947661d4a8154cd21af16
BrianOtieno12 <noriamy52@gmail.com>
2022-02-28 07:14:54 +0300

    Basic Syntax

---
+++ basicSyntax.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>There are three types of php tags</h1>
+    <ul>
+        <li>1. normal Tag </li>
+        <li>2. short echo tag</li>
+        <li>3. short tag</li>
+    </ul>
+
+    <ul>Examples
+        <li>Normal Tag
+        <?php echo "Hello World";?>
+        </li>
+        <li>short echo tag
+            <?= "Hello World" ?>
+        </li>
+        <li>short tag
+            <? "Hello world" ?>
+        </li>
+    </ul>
+
+    <h2>Types of comments</h2>
+    <ul>
+        <li>1. Single line '//'</li>
+        <li>2. Multi line '/*....*/'</li>
+    </ul>
+       
+</body>
+
+</html>
