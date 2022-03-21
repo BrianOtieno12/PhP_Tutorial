f18a49abcb5d60bf183f18d9128eb84227dfaf6e
BrianOtieno12 <noriamy52@gmail.com>
2022-03-21 17:19:46 +0300

    Variables

---
+++ phpPractioner/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Variables
+ */
+
+$name = 'BrianOtieno12';
+
+//variables can be nested inside a string when using double quotes
+echo "Hello, $name"."<br/>";
+
+echo "Hello, {$name}"."<br/>";
+
+//Variables are concatenated when using single quotes
+echo 'Hello, ' . $name;
