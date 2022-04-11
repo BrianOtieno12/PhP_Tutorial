662b4cbebeb9bb7171149964ca135cb341e6da76
BrianOtieno12 <noriamy52@gmail.com>
2022-04-11 06:26:03 +0300

    Intergers php documentation

---
+++ Types/integers.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * int size can be determined using PHP_INT_SIZE
+ * maximum value using PHP_INT_MAX
+ * minimum value using PHP_INT_MIN
+ * If PHP encounters a number beyond the bounds of the int type, it will be interpreted as a float instead. 
+ * To explicitly convert a value to int, use either the (int) or (integer) casts
+ * When converting from float to int, the number will be rounded towards zero. 
+ * Integer literals may be decimal, octal, hexadecimal or binary
+ */
